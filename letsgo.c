e1df518f583723c9a0c09e3516c46214869b6769
yessinrakai <rakaiyessin@gmail.com>
2021-11-17 11:22:06 +0100

    Added comment from rep2

--- letsgo.c
+++ letsgo.c
@@ -4,11 +4,7 @@ int main(){
 
 printf("Git, let's go!!\n");
 printf("added line 1\n");
-<<<<<<< HEAD
 printf("Working on a new feature 1...\n");
-=======
-printf("working on a new feature 1...\n");
->>>>>>> 605d4a97fd7d6f6457af3b570d90fe33739c9936
 return 0;
 }
 /*Added comment from repository 2(machine2)*/
